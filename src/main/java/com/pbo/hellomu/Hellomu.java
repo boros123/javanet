8361986f197e00e5ac3967967f91b78e82fc1029
boros123 <nauvaall.qa@gmail.com>
2024-02-01 18:45:33 +0700

    pertemuan 1

---
+++ src/main/java/com/pbo/hellomu/Hellomu.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pbo.hellomu;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Hellomu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello ");
+        System.out.println("Hello World!");
+    }
+}
